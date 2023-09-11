437de765723f55f2c939930abebe223d69e47122
newerk <juniusowens@yahoo.com>
2023-09-11 16:24:52 -0400

    Add Component That Shows Compact View of Education

--- react-app/src/components/form-education.jsx
+++ react-app/src/components/form-education.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { savedEducationData } from "../data/savedEducation";
 import DateRange from "./date-range";
 
 function Degree() {
@@ -16,21 +18,44 @@ function School() {
     <>
       School / University *optional*
       <label htmlFor="school">
-      <input type="text" name="school" id="school" />
+        <input type="text" name="school" id="school" />
       </label>
     </>
   );
 }
 
 export default function EducationComponent() {
+  const [savedEducation, setSavedEducation] = useState(savedEducationData);
+
+  function SavedEducationList() {
+    return (
+      <>
+        {savedEducation.map((education) => (
+          <div key={education.id} className="saved-education">
+            <p>
+              <strong>{education.degree}</strong>, {education.school}
+            </p>
+            <p>
+              {education.location} | {education.endDate.year}
+            </p>
+          </div>
+        ))}
+      </>
+    );
+  }
+
   return (
     <>
       <h1>
         Education <span>*optional*</span>
       </h1>
-      <School />
-      <Degree />
-      <DateRange />
+      <SavedEducationList />
+      <button>+ New Education</button>
+      <div>
+        <School />
+        <Degree />
+        <DateRange />
+      </div>
     </>
   );
 }
---
+++ react-app/src/data/savedEducation.js
@@ -0,0 +1,18 @@
+export let savedEducationData = [
+  {
+    id: 0,
+    school: "University at Buffalo",
+    degree: "Geographic Information Systems (GIS)",
+    location: "Buffalo, NY",
+    beginDate: { month: "January", year: "2017" },
+    endDate: { month: "December", year: "2020", present: false },
+  },
+  {
+    id: 1,
+    school: "University at Albany",
+    degree: "Computer Science",
+    location: "Albany, NY",
+    beginDate: { month: "August", year: "2016" },
+    endDate: { month: "December", year: "2016", present: false },
+  },
+];
