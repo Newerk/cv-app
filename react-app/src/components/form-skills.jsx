3353a8f9dac17b11d7479a397c2763dafa31e2b4
newerk <juniusowens@yahoo.com>
2023-09-15 15:22:10 -0400

    Complete All Features of Skills Component

--- react-app/src/components/form-skills.jsx
+++ react-app/src/components/form-skills.jsx
@@ -3,28 +3,55 @@ import { useRef, useState } from "react";
 export default function SkillsComponent() {
   const [savedSkills, setSavedSkills] = useState("");
   const skillsRef = useRef(null);
+  const skillsContainerRef = useRef(null);
+  const addSkillBtnRef = useRef(null);
 
   return (
     <div className="skills-section">
       <h1>Skills</h1>
-      {savedSkills}
-      <label htmlFor="">
-        <textarea
-          ref={skillsRef}
-          name=""
-          id=""
-          cols="30"
-          rows="5"
-          placeholder="Seperate each skill with a comma ','"
-        ></textarea>
-      </label>
+      <p>{savedSkills}</p>
       <button
-        onClick={() => {
-          setSavedSkills(skillsRef.current.value);
+        ref={addSkillBtnRef}
+        onClick={(e) => {
+          skillsContainerRef.current.classList.toggle("hidden");
+          e.target.classList.toggle("hidden");
         }}
       >
         + Add Skills
       </button>
+
+      <div ref={skillsContainerRef} className="skills-container hidden">
+        <label htmlFor="skills-textbox">
+          <textarea
+            ref={skillsRef}
+            name="skills-textbox"
+            id="skills-textbox"
+            cols="30"
+            rows="5"
+            placeholder="Seperate each skill with a comma ','"
+          ></textarea>
+        </label>
+        <button
+          className="skills cancel-btn"
+          onClick={() => {
+            skillsRef.current.value = savedSkills;
+            skillsContainerRef.current.classList.toggle("hidden");
+            addSkillBtnRef.current.classList.toggle("hidden");
+          }}
+        >
+          Cancel
+        </button>
+        <button
+          className="skills save-btn"
+          onClick={() => {
+            setSavedSkills(skillsRef.current.value);
+            skillsContainerRef.current.classList.toggle("hidden");
+            addSkillBtnRef.current.classList.toggle("hidden");
+          }}
+        >
+          Save
+        </button>
+      </div>
     </div>
   );
 }
